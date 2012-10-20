b354483dabff714e18ff62dc22d78b0ffaaa08b5
Kamil Essekkat <kamil@essekkat.pl>
2012-10-20 21:58:59 +0200

    Fixed path in local deploy. Added new makefiles to ignore list.

--- _config.py
+++ _config.py
@@ -19,7 +19,7 @@
 #  If you're hosting a blogofile powered site as a subdirectory of a larger
 #  non-blogofile site, then you would set the site_url to the full URL
 #  including that subdirectory: "http://www.yoursite.com/path/to/blogofile-dir"
-site.url = "http://essekkat.pl"
+site.url = "http://essekkat.pl "
 
 #### Blog Settings ####
 blog = controllers.blog
@@ -66,7 +66,7 @@ site.file_ignore_patterns = [
 ".*/\.(git|hg|svn|bzr)$",
 ".*/.(git|hg)ignore$",
 ".*/CVS$",
-".*/Makefile$",
+".*/Makefile*$",
 ".*/site.tar.gz$",
 ]
 
--- projekty.html.mako
+++
@@ -1,4 +0,0 @@
-<%inherit file="_templates/site.mako" />
-<h2>Aktualne:</h2>
-
-<h2>Już nie:</h2>
