c4c710450dcfda1c59f41d4ed6a1a630ff7ae8ed
Kamil Essekkat <kamil@essekkat.pl>
2014-02-21 10:31:31 +0100

    prod ready analytics

--- app/google-analytics.js
+++ app/google-analytics.js
@@ -3,6 +3,5 @@
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) 
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); 
 
-    // ga('create', 'UA-48118936-1', 'essekkat.pl');
-    ga('create',    'UA-XXXXXXXX-Y', 'essekkat.pl');
+    ga('create', 'UA-48118936-1', 'essekkat.pl');
     ga('send', 'pageview');
